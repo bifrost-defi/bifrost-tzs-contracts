25ef808431d41d944a69803dc7aa19664ddf0d4b
AlexeySadkovich <sadkovich01@mail.ru>
2022-03-10 13:36:55 +0300

    refactor: update orgination script

--- orginate.ts
+++ orginate.ts
@@ -1,23 +1,23 @@
 import { TezosToolkit, MichelsonMap } from "@taquito/taquito";
 import { importKey } from "@taquito/signer";
+import fs from "fs";
 
 require("dotenv").config();
 
-import contractJSON from "../build/contract.json";
-
-const provider = "http://127.0.0.1:20000";
+const provider: string = process.env.RPC_URL || "";
+const pk: string = process.env.PRIVATE_KEY || "";
 
 async function deploy() {
   const tezos = new TezosToolkit(provider);
 
-  await importKey(tezos, process.env.PRIVATE_KEY);
+  await importKey(tezos, pk);
 
   try {
     const op = await tezos.contract.originate({
-      code: contractJSON,
+      code: fs.readFileSync("./build/contract.tz").toString(),
       storage: {
         owner: process.env.OWNER,
-        supply: 0,
+        totalSupply: "0",
         ledger: new MichelsonMap(),
         metadata: new MichelsonMap(),
         burnings: new MichelsonMap(),
@@ -27,6 +27,7 @@ async function deploy() {
     console.log("Awaiting confirmation...");
     const contract = await op.contract();
 
+    console.log("Address", contract.address);
     console.log("Gas Used", op.consumedGas);
     console.log("Storage", await contract.storage());
 
